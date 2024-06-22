b61d9e5236645cf34d65e5245cc2ab0532e7af19
dimasixc22 <dimasixc22@gmail.com>
2024-06-22 12:10:47 +0000

    feat : "bg"

--- src/components/atoms/search.jsx
+++ src/components/atoms/search.jsx
@@ -1,5 +1,5 @@
 export default function Search(){
     return(
-        <input type="text" name="" id="" style={{maxWidth: "300px", width: "100%", borderRadius: "4px", border: "darkgray 1px solid"} }/>
+        <input type="text" style={{maxWidth: "300px", width: "100%", borderRadius: "4px", border: "darkgray 1px solid"} }/>
     )
 }
